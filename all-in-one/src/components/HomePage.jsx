cd66ab7a64788b5e70603a14a3c461578af43f64
d0s0jfn <d0s0jfn@homeoffice.wal-mart.com>
2023-01-07 12:42:58 +0530

    Code Refactor

--- all-in-one/src/components/HomePage.jsx
+++ all-in-one/src/components/HomePage.jsx
@@ -1,31 +1,29 @@
 import { Fragment } from "react"
 import Checkbox from "@material-ui/core/Checkbox";
-import Stack from "react-bootstrap/Stack";
+import { Stack} from "@mui/material";
 
 export const goals = [1,2,3,4]
 
 const HomePage = () => {
     return (
-      <Fragment>
-        <div style={{ align: "center", paddingTop: "80px" }}>
+      <div style={{align:'center'}}>
+        <div style={{ paddingTop: "80px" }}>
           <h1>My Goals</h1>
         </div>
         <div>
-          <Stack direction="horizontal" spacing={2}>
-            <>
+          <Stack direction="row" spacing={2}>
               <Checkbox
                 defaultChecked
                 color="primary"
                 inputProps={{ "aria-label": "secondary checkbox" }}
               />
-            </>
             <h2>Goal 1</h2>
           </Stack>
         </div>
         <div>
           <h3>Details</h3>
         </div>
-      </Fragment>
+      </div>
     );
 }
 
